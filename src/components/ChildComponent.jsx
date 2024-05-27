23243077528146f979d8f4b60ec1d11da2be25fe
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2024-05-27 22:48:08 +0700

    add JS default params and react icons

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -3,20 +3,23 @@ import PropTypes from "prop-types";
 
 const ChildComponent = (props) => {
 	return (
-		<div className="bg-orange-300 shadow-md p-4 rounded-lg mb-4 font-semibold text-md">
-			<h2 className="text-lg font-bold mb-2">Child Component</h2>
-			<p className="text-gray-700">Name: {props.name}</p>
-			<p className="text-gray-700">Age: {props.age}</p>
+		<div className="p-4 mb-4 font-semibold bg-orange-300 rounded-lg shadow-md text-md">
+			<h2 className="mb-2 text-xl font-bold">Child Component</h2>
+			<p className="text-lg text-gray-700">Name: {props.name}</p>
+			<p className="text-lg text-gray-700">Age: {props.age}</p>
+			<p className="text-lg text-gray-700">Location: {props.location}</p>
 		</div>
 	);
 };
 
-// const ChildComponent = ({ name, age }) => {
+//// to set a default prop is by destructuring the props object
+// const ChildComponent = ({ name, age, location = "No Location" }) => {
 // 	return (
-// 		<div className="bg-orange-300 shadow-md p-4 rounded-lg mb-4 font-semibold text-md">
-// 			<h2 className="text-lg font-bold mb-2">Child Component</h2>
+// 		<div className="p-4 mb-4 font-semibold bg-orange-300 rounded-lg shadow-md text-md">
+// 			<h2 className="mb-2 text-lg font-bold">Child Component</h2>
 // 			<p className="text-gray-700">Name: {name}</p>
 // 			<p className="text-gray-700">Age: {age}</p>
+// 			<p className="text-gray-700">Location: {location}</p>
 // 		</div>
 // 	);
 // };
@@ -24,6 +27,9 @@ const ChildComponent = (props) => {
 ChildComponent.propTypes = {
 	name: PropTypes.string.isRequired,
 	age: PropTypes.number.isRequired,
+	location: PropTypes.string,
 };
+
+// Support for defaultProps will be removed from function components in a future major release. Use JavaScript default parameters instead
 
 export default ChildComponent;
